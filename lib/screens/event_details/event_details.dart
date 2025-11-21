df952596098fd6c3c4714642dcc77d8d21112006
Benjamin Revelo <benjaminrevelo@Benjamins-Air.attlocal.net>
2025-11-21 11:26:42 -0500

    added the structure of the project

---
+++ lib/screens/event_details/event_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventDetails extends StatefulWidget {
+  const EventDetails({super.key});
+
+  @override
+  State<EventDetails> createState() => _EventDetailsState();
+}
+
+class _EventDetailsState extends State<EventDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
